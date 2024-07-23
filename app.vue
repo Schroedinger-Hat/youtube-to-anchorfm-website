a92dd5f37d7011b0d08d97b1cb25a8a2c8587902
Patrick Raedler <code@readpato.dev>
2024-07-23 19:35:57 +0200

    refactor(app): streamline everything from root, add better grid type

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Youtube to Anchor.fm',
+  description: 'An automation tool for your podcast',
+})
+</script>
+
 <template>
-  <SHWelcome />
+  <div class="app grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-1 h-dvh">
+    <TheGreeting class="row-span-1" />
+    <div class="row-span-3 md:row-span-1 w-full bg-white flex flex-col justify-center items-center p-4">
+      <NuxtPage />
+    </div>
+  </div>
 </template>
