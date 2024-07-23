0e1646a6e0257ec5b9b55fa1066d7c157cd8fa46
Patrick Raedler <code@readpato.dev>
2024-07-23 19:23:02 +0200

    refactor(LogoIntro): remove gsap, make static

---
+++ components/LogoIntro.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SpotifyLogo from '~/assets/svg/spotify-logo.svg'
+import YoutubeLogo from '~/assets/svg/youtube-logo.svg'
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center w-full gap-2">
+    <div class="flex justify-center items-end gap-3 text-4xl font-bold">
+      <div class="flex flex-col justify-center items-center space-y-1">
+        <YoutubeLogo class="size-8" />
+        <h1>Youtube</h1>
+      </div>
+      <p>to</p>
+      <div class="flex flex-col justify-center items-center space-y-1">
+        <SpotifyLogo class="text-[#AFB1FF] size-8" />
+        <h1>Anchor.fm</h1>
+      </div>
+    </div>
+    <h2 class="scroll-m-20 text-xl font-semibold tracking-tight">
+      An automation tool to publish your podcast
+    </h2>
+  </div>
+</template>
