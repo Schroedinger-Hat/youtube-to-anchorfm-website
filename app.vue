d420a8722d0ac340674e35b93442d31a8051088b
Patrick Raedler <code@readpato.dev>
2024-08-15 19:48:01 +0200

    style(app.vue): add classes to adjust text styling

--- app.vue
+++ app.vue
@@ -8,8 +8,14 @@ useSeoMeta({
 <template>
   <div class="app grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-1 h-dvh">
     <TheGreeting class="row-span-1" />
-    <div class="row-span-3 md:row-span-1 bg-white flex flex-col justify-center items-center p-4 text-black w-full">
-      <ContentDoc class="h-dvh" />
+    <div class="row-span-3 md:row-span-1 bg-white flex flex-col justify-center items-center p-4 text-black w-full overflow-y-scroll text-wrap">
+      <ContentDoc class="h-dvh w-full" />
     </div>
   </div>
 </template>
+
+<style lang="css">
+p + p:not(:last-child) {
+  margin-bottom: 16px;
+}
+</style>
