5af806ce0a573a7a59c3b678af5805935a2c2d0c
Patrick Raedler <code@readpato.dev>
2024-08-15 19:47:25 +0200

    feat(content/ProseA): replace content component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  href: string
+  target?: '_blank' | '_parent' | '_self' | '_top' | (string & {}) | null | undefined
+}>(), {
+  href: '',
+  target: undefined,
+})
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    class="underline underline-offset-2 decoration-gray-400 hover:decoration-gray-700 transition-colors"
+  >
+    <slot />
+  </NuxtLink>
+</template>
